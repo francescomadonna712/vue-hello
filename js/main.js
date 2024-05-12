9e12935c4ec28f4bd183a9162ab8f1ad89f45590
francesco madonna <fra.mad.93@hotmail.it>
2024-05-12 19:38:42 +0200

    inserisco img

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            linkImg: "https://f1grandprix.motorionline.com/wp-content/uploads/2024/05/livree-ferrari-gp-miami-2024-1024x691.jpg"
         }
     }
 }).mount('#app')
